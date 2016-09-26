54c5d1de5e97f349994116ac25d9e1c5e38fc6f1
Freya Helstrup <freyagh@me.com>
2016-09-26 15:55:25 +0200

    Update Dice.java

--- Dice.java
+++ Dice.java
@@ -4,14 +4,14 @@ public class Dice {
 	// roll the die and return the value (1-6)
 	public int roll() {
 		float d1=(float)Math.random();     // 0-1
-		float d2=d1*5;                     // 0-5
+		float d2=(float)(d1*6-0.5);        // -0.5 - 5.5
 		int d3=Math.round(d2);             // 0-5 integer
 		return d3 + 1;                     // 1-6		
 	}
 	
 	// roll the die n times and print the values
 	public void rollMultiple(int n) {
-		for (int i=1; i<n; i++) {
+		for (int i=1; i<=n; i++) {
 			System.out.print(roll() + " ");
 		}		
 	}
